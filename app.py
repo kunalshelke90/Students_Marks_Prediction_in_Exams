d6b569990c9a54478cee487f77a0ee834613f5ba
kunalshelke90 <shelkekunal90@gmail.com>
2024-08-10 00:55:06 +0530

    Model Trainer completed with mlflow

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion,DataIngestionConfig 
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainer,ModelTrainerConfig
 import sys
 
 # if __name__=="__ main__ ":
@@ -13,8 +14,11 @@ try:
     
     # data_transformation_config=DataIngestionConfig()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
     
+    
+    model_trainer=ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr,test_arr)
 except Exception as e:
     logging.info("Custom Exception")
     raise CustomException(e,sys)
